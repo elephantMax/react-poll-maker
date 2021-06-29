dc120d2a24be6342104197cbbe0379af2f5b22d8
elephantMax <hatypovm12@mail.ru>
2021-06-29 15:37:47 +0300

    added react-hook-form and validation in Create form

--- src/components/PollDetils.js
+++ src/components/PollDetils.js
@@ -4,6 +4,7 @@ import { fetchPollById, setPoll, setVoteLoading, vote } from '../store/slices/po
 import { useEffect, useState } from 'react';
 import useDateDifference from '../hooks/useDateDifference';
 import Loader from './Loader';
+import { useForm } from 'react-hook-form';
 
 const PollDetails = () => {
     const { poll, pollLoading, voteLoading } = useSelector((state) => state.poll)
@@ -11,13 +12,14 @@ const PollDetails = () => {
     const [selectedOption, setSelectedOption] = useState()
     const { id } = useParams()
     const history = useHistory()
+    const { setError, formState: { errors } } = useForm()
 
     const submitHandler = (e) => {
         e.preventDefault()
         if (selectedOption) {
             dispatch(vote({ poll, optionId: selectedOption }))
         }
-        // show message no selected
+        setError('options')
     }
 
     const dateDifference = useDateDifference(poll)
@@ -65,6 +67,7 @@ const PollDetails = () => {
                                             {option.text}
                                         </label>
                                     </div>)}
+                                    {errors.options && <span className="form__error">Choose one of this answers</span>}
                                 <div className="form-answer__actions">
                                     <button className="btn btn-green">Vote</button>
                                     <Link className="btn btn-dark" to={`/results/${id}`}>
