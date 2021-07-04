d37ba99925935abb2b42ca85869909190d17fe4b
elephantMax <hatypovm12@mail.ru>
2021-07-04 15:31:41 +0300

    fixed loaders

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
@@ -12,8 +13,15 @@ const Profile = () => {
     const { id } = useParams()
 
     useEffect(() => {
-        dispatch(getUserById({id}))
+        dispatch(getUserById({ id }))
     }, [id, profileData, dispatch])
+
+    const userPolls = useMemo(() => {
+        if (polls) {
+            return polls.filter(poll => poll.user_id === id)
+        }
+        return []
+    }, [polls, id])
 
     return (
         <div className="page">
@@ -26,9 +34,9 @@ const Profile = () => {
                         </h2>
                     </div>
                     <h2 className="subtitle">Polls</h2>
-                    <PollsList polls={polls} />
+                    <PollsList polls={userPolls} />
                 </>
-            ) : <Loader /> }
+            ) : <Loader />}
         </div>
     );
 }
